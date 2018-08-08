8d7a60a1af3eb4e2d0856eb9e2f5006374f859d4
dorofeev <dorofeev@actimind.com>
2018-08-08 11:56:31 +0300

    remove unused

--- app/routes/usersRouter.js
+++ app/routes/usersRouter.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const Util = require('../util/Util')
-const ResponseUtil = require("../util/ResponseUtil")
 const userController = require("../controllers/usersController")
 
 // GET users listing
